948ebeb342d89571f3601594609a0c2c9641f466
tohzhenkang <tohzhenkang@gmail.com>
2025-08-16 14:48:56 +0800

    FE: add tests for check weather button, ensure able to get response

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
 from django.apps import apps
 
+
 class SanityTestCase(TestCase):
 	def test_homepage_app_loaded(self):
 		"""Sanity check: homepage app should be loaded."""
 		self.assertTrue(apps.is_installed('homepage'))
+
+
+class WeatherViewTests(TestCase):
+	def test_get_homepage_renders_input_and_button(self):
+		"""GET / should render the input field and button."""
+		response = self.client.get('/')
+		self.assertContains(response, 'name="location"')
+		self.assertContains(response, 'Check Weather')
+
+	def test_get_homepage_with_location_shows_weather(self):
+		"""GET /?location=London should show weather status for London."""
+		response = self.client.get('/', {'location': 'London'})
+		# Should still show the input field and button for further queries
+		self.assertContains(response, 'name="location"')
+		self.assertContains(response, 'Check Weather')
+		# Should show the location somewhere in the weather result
+		self.assertContains(response, 'London')
